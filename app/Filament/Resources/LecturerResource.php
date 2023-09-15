8f39c5d502a1a1d887b2b60530cc090a5ac46dcd
Zulfiqry Saadputra <zsp301@gmail.com>
2023-09-15 11:38:30 +0800

    disable edit on row click lecturer list page

--- app/Filament/Resources/LecturerResource.php
+++ app/Filament/Resources/LecturerResource.php
@@ -52,6 +52,7 @@ class LecturerResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->recordUrl(null)
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->numeric()
