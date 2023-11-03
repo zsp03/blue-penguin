be2122947ecddde96e8bea45279ec1b727a8cc24
zsp03 <zsp301@gmail.com>
2023-11-03 11:09:41 +0800

    add PublicationRelationManager to LecturerResource

--- app/Filament/Admin/Resources/LecturerResource.php
+++ app/Filament/Admin/Resources/LecturerResource.php
@@ -180,7 +180,7 @@ class LecturerResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\PublicationsRelationManager::class
         ];
     }
 
