cafee71d4db2ca99b9f15eec5ad7e7b61a40a8c7
Firmansyah Jaya Kusuma <88803824+Firmansjk@users.noreply.github.com>
2023-11-13 09:47:57 +0800

    fix hakitype uncategorized

--- app/Enums/HakiType.php
+++ app/Enums/HakiType.php
@@ -13,6 +13,7 @@ enum HakiType: string implements HasLabel
     case INDIKASI_GEOGRAFIS = 'indikasi_geografis';
     case RAHASIA_DAGANG = 'rahasia_dagang';
     case DESAIN_TATA_LETAK_SIRKUIT_TERPADU = 'desain_tata_letak_sirkuit_terpadu';
+    case UNCATEGORIZED = 'Uncategorized';
 
     public function getLabel(): ?string
     {
@@ -23,7 +24,8 @@ enum HakiType: string implements HasLabel
             self::HAK_CIPTA => __('Hak Cipta'),
             self::INDIKASI_GEOGRAFIS => __('Indikasi Geografis'),
             self::RAHASIA_DAGANG => __('Rahasia Dagang'),
-            self::DESAIN_TATA_LETAK_SIRKUIT_TERPADU => __('Desain Tata Letak Sirkuit Terpadu')
+            self::DESAIN_TATA_LETAK_SIRKUIT_TERPADU => __('Desain Tata Letak Sirkuit Terpadu'),
+            self::UNCATEGORIZED => __('Uncategorized')
         };
     }
 }
